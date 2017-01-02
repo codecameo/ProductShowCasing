892e7a2a56f29890bfa434baab2ff15ade427c24
Sharif <sharifur.rahman@gagagugu.com>
2017-01-02 22:26:30 +0600

    add initial drawer.

--- app/src/main/java/bytes/wit/showcasing/HomeActivity.java
+++ app/src/main/java/bytes/wit/showcasing/HomeActivity.java
@@ -1,11 +1,17 @@
 package bytes.wit.showcasing;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,13 +19,14 @@ import java.util.List;
 import bytes.wit.fragments.FragmentHome;
 import bytes.wit.fragments.FragmentPopular;
 
-public class HomeActivity extends BaseActivity {
+public class HomeActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private ViewPager mHomeViewPager;
     private List<String> mFragmentTitleList;
     private List<Fragment> mFragmentList;
     private HomeViewPagerAdapter mPagerAdapter;
     private TabLayout mTabLayout;
+    private DrawerLayout mDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +36,29 @@ public class HomeActivity extends BaseActivity {
         initViews();
         initVariables();
         initViewPager();
+
+        initNavigationDrawer();
     }
+
+    private void initNavigationDrawer() {
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, mDrawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawer.addDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
 
     private void initVariables() {
         mFragmentList = new ArrayList<>();
@@ -40,6 +69,8 @@ public class HomeActivity extends BaseActivity {
     private void initViews() {
         mHomeViewPager = (ViewPager) findViewById(R.id.vp_home);
         mTabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
     }
 
     private void initViewPager() {
@@ -50,6 +81,22 @@ public class HomeActivity extends BaseActivity {
         mHomeViewPager.setAdapter(mPagerAdapter);
         mTabLayout.setupWithViewPager(mHomeViewPager);
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            // Handle the camera action
+        } else if (id == R.id.nav_language) {
+
+        }
+
+        mDrawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
 
     private class HomeViewPagerAdapter extends FragmentPagerAdapter {
         public HomeViewPagerAdapter(FragmentManager supportFragmentManager) {
